e30a4602f8df061dc17abda000b6806c843b0d83
Casey Pugh <me@caseypugh.com>
2015-05-15 21:52:01 -0400

    mansplainr v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Splainr::Application.config.secret_token = 'dc1aeb11aae503409a037e6d76547d8a516b3c18288afe16867b5b0780e4fb65cabcbe434c0b987e135e31e04056d04c06d7512fc135421483800bfff6a5e5dc'
