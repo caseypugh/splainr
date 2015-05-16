858aa159498f27f7ad6d2d467a85468474958ce8
Casey Pugh <me@caseypugh.com>
2015-05-16 14:06:35 -0400

    breakout into new lines

--- app/workers/splainr_worker.rb
+++ app/workers/splainr_worker.rb
@@ -5,8 +5,10 @@ class SplainrWorker
     # number = Number::create_from_number!(phone_number, message)
     translation = SplainrTranslator::execute(message)
 
-    TwilioWorker.preform_async(phone_number, translation)
     # Rails.logger.debug message
     # Rails.logger.debug translation
+    TwilioWorker.preform_async(phone_number, translation)
+    
+    # TwilioWorker.perform_in(2.seconds, phone_number, translation)
   end
 end
--- app/workers/twilio_worker.rb
+++ app/workers/twilio_worker.rb
@@ -5,9 +5,9 @@ class TwilioWorker
     @client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_AUTH_TOKEN'] 
  
     @client.account.messages.create({
-      :from => '+19177467982', 
-      :to => phone_number, 
-      :body => translation,  
+      from: '+19177467982', 
+      to: phone_number, 
+      body: translation
     })
   end
 end
