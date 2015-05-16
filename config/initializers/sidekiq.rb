e82a5ea0f2ee00c35ab3338a789bb9befc8a1d58
Casey Pugh <me@caseypugh.com>
2015-05-15 23:06:50 -0400

    use sidekiq

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,25 @@
+require 'sidekiq/pro/reliable_push'
+
+provider = ENV.fetch('SIDEKIQ_REDIS_PROVIDER', 'SIDEKIQ_REDIS_URL')
+url      = ENV[provider] || ENV['SIDEKIQ_REDIS_URL'] || ENV['REDIS_URL']
+timeout  = Float(ENV.fetch('REDIS_TIMEOUT', 0.5))
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+    url:          url,
+    size:         Integer(ENV.fetch('REDIS_POOL', 1)),
+    timeout:      timeout,
+    pool_timeout: timeout
+  }
+end
+
+Sidekiq.configure_server do |config|
+  require 'sidekiq/pro/reliable_fetch'
+  config.poll_interval = 10
+  config.redis = {
+    url: url,
+    size: Sidekiq.options[:concurrency] + 2
+  }
+end
+
+Sidekiq.default_worker_options = { queue: 'medium', backtrace: true }
