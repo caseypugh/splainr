52a3526b7d1c077e62718be00bfb841355590f2b
Casey Pugh <me@caseypugh.com>
2015-05-16 10:26:16 -0400

    twilio response working

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,18 @@ class HomeController < ApplicationController
   def index
 
   end
+
+  def mansplainr_response
+    # response = Splainr::define(params[])
+    # sender = params[:From]
+    # friends = {
+    #   "+14153334444" => "Curious George",
+    #   "+14158157775" => "Boots",
+    #   "+14155551234" => "Virgil"
+    # }
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Hello. Thanks for the message."
+    end
+    @response = twiml.text
+  end
 end
